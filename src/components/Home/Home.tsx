680e21142f70e1d4a301afde47d23cc1652f9fa4
NecroBread <mariovyordanov@gmail.com>
2022-08-19 12:21:36 +0300

    Add home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { GiMicrochip } from 'react-icons/gi';
+import { GiHumanTarget } from 'react-icons/gi';
+import styles from './Home.module.css';
 
 export class Home extends Component {
 	render() {
 		return (
-			<div>Home</div>
+			<div className={styles.wrapper}>
+				<div className={styles.games}>
+					<Link className={styles['game-link']} to="/game/computer">
+						<div><GiMicrochip size={'100px'} /></div>
+						<div>Play vs Artificial Intelligence</div>
+					</Link>
+					<div className={styles['middle-line']}></div>
+					<Link className={styles['game-link']} to="/game/computer">
+						<GiHumanTarget size={'100px'} />
+						<div></div>
+						<div>
+							Play vs Real Players
+						</div>
+					</Link>
+				</div>
+			</div>
 		)
 	}
 }
