b994f6564c42d31541404e5df0f7e266c5fd8bcd
NecroBread <mariovyordanov@gmail.com>
2022-08-22 10:39:11 +0300

    Design change

--- src/components/Game/gameComponents/PlayerCard/PlayerCard.tsx
+++ src/components/Game/gameComponents/PlayerCard/PlayerCard.tsx
@@ -13,9 +13,8 @@ export default class PlayerCard extends Component<Props> {
 			<div className={style['player-card']}>
 				<h3 className={style['player-name']}>{this.props.playerName}</h3>
 				<div className={`${style['turn-card']} ${this.props.yourTurn ? style.active : null}`}>
-					<p className={`${style['turn']}`}>your turn</p>
+					<p className={`${style['turn']}`}>{this.props.sign === 'x' ? '✖' : '○'} turn</p>
 				</div>
-				<div className={style.sign}>{this.props.sign}</div>
 			</div>
 		)
 	}
