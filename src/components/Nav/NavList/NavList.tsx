0ac25fd00fbd6d0066f64897fa948359a04ee883
NecroBread <mariovyordanov@gmail.com>
2022-08-23 12:21:01 +0300

    Logout btn temporary styles

--- src/components/Nav/NavList/NavList.tsx
+++ src/components/Nav/NavList/NavList.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { AuthLib } from '../../../utils/AuthLib';
 import './NavList.css';
 import { app } from '../../../configs/firebase.config';
@@ -10,8 +10,10 @@ export default class NavList extends Component {
 			<>
 				<li><NavLink to="/" >Home</NavLink></li>
 				<li><NavLink to="/profile" >Profile</NavLink></li>
-				<li><span onClick={() =>
-					AuthLib.handleLogout(app)}>Logout</span></li>
+				<li><a href='/logout' className='logoutBtn' onClick={(e) => {
+					e.preventDefault();
+					AuthLib.handleLogout(app)
+				}}>Logout</a></li>
 			</>
 		)
 	}
