163dc983ff895249795aa279e864d0aaf07ead40
NecroBread <mariovyordanov@gmail.com>
2022-08-25 11:10:34 +0300

    Create PvP game in DB

--- src/components/Game/GamePvP/PvPMenu/PvPMenu.tsx
+++ src/components/Game/GamePvP/PvPMenu/PvPMenu.tsx
@@ -1,27 +1,94 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import { Link as div, Navigate } from 'react-router-dom';
 import { IoIosLogIn } from 'react-icons/io';
 import { IoIosCheckboxOutline } from 'react-icons/io';
+import { auth, db } from '../../../../configs/firebase.config';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import Spinner from '../../../common/Spinner/Spinner';
 
-export class PvPMenu extends Component {
+interface IState {
+	gameId: string,
+	loading: boolean,
+}
+
+export class PvPMenu extends Component<any, IState> {
+	constructor({ }) {
+		super({});
+		this.state = {
+			gameId: '',
+			loading: false,
+		}
+	}
+
+
+	handleCreateGame() {
+		this.setState({
+			loading: true,
+		})
+
+		const user = auth.currentUser
+
+		const sign1 = Math.floor((Math.random() * 2)) >= 0.5 ? 'x' : 'o';
+		const sign2 = sign1 === 'x' ? 'o' : 'x';
+
+		if (user) {
+			const data = {
+				owner: user.uid,
+				mode: 'pvp',
+				open: true,
+				history: JSON.stringify([Array(9).fill(undefined)]),
+				playersIds: [user.uid, ''],
+				playerDisplayNames: [user.displayName || 'Anonymous', ''],
+				playerSigns: [sign1, sign2],
+				createdAt: serverTimestamp(),
+			}
+
+			const ref = collection(db, "activeGames");
+			addDoc(ref, data)
+				.then((doc) => {
+					this.setState({
+						gameId: doc.id,
+					})
+				})
+				.catch(err => {
+					console.log(err);
+				})
+				.finally(() => {
+					this.setState({
+						loading: false,
+					})
+				})
+		}
+
+	}
+
 	render() {
-		return (
-			<div className={'games-wrapper'}>
-				<div className={'games'}>
-					<Link className={'game-link'} to="/game/PvE">
-						<div><IoIosCheckboxOutline size={'100px'} /></div>
-						<div>Create game</div>
-					</Link>
-					<Link className={'game-link'} to="/game/PvP">
-						<IoIosLogIn size={'100px'} />
-						<div></div>
-						<div>
-							Join game
+		if (this.state.gameId === '') {
+			return (
+				<div className={'games-wrapper'}>
+					{this.state.loading
+						? <div className='absolute'>
+							<Spinner />
 						</div>
-					</Link>
+						: null}
+					<div className={'games'}>
+						<div className={'game-link'} onClick={this.handleCreateGame.bind(this)}>
+							<div><IoIosCheckboxOutline size={'100px'} /></div>
+							<div>Create game</div>
+						</div>
+						<div className={'game-link'}>
+							<IoIosLogIn size={'100px'} />
+							<div></div>
+							<div>
+								Join game
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-		)
+			)
+		} else {
+			return <Navigate to={`/game/PvP/${this.state.gameId}`} />
+		}
 	}
 }
 
