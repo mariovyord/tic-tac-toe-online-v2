a3651165a7dd5b708faf012b4a3ba1d7a7808705
NecroBread <mariovyordanov@gmail.com>
2022-08-31 15:18:25 +0300

    change: common layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,14 @@ import { NotificationProvider } from './contexts/NotificationContext';
 import { Compose } from './utils/Compose';
 import { PrivateRoutes } from './utils/PrivateRoutes';
 import { PrivacyPolicy } from './components/PrivacyPolicy/PrivacyPolicy';
+import GamePvE from './components/Game/GamePvE/GamePvE'
+import { GamePvPWithRouter } from './components/Game/GamePvP/GamePvP/GamePvP'
+import { GamesListWithRouter } from './components/Game/GamePvP/GamesList/GamesList'
+import PvPMenu from './components/Game/GamePvP/PvPMenu/PvPMenu'
+import { ReviewGameWithRouter } from './components/Game/ReviewGame/ReviewGame'
+import Home from './components/Home/Home'
+import NotFound from './components/NotFound/NotFound'
+import Profile from './components/Profile/Profile'
 
 interface IState {
 	authenticated: boolean,
@@ -41,7 +49,18 @@ export default class App extends Component<any, IState> {
 						<Route path='/signin' element={<Auth authenticated={this.state.authenticated} />} />
 						<Route path='/privacypolicy' element={<PrivacyPolicy />} />
 						<Route element={<PrivateRoutes authenticated={this.state.authenticated} />}>
-							<Route path="*" element={<CommonLayout />} />
+							<Route element={<CommonLayout />}>
+								<Route path='/' element={<Home />} />
+								<Route path='/profile' element={<Profile />} />
+								<Route path='/game' >
+									<Route path="PvE" element={<GamePvE />} />
+									<Route path="PvP" element={<PvPMenu />} />
+									<Route path="PvP/list" element={<GamesListWithRouter />} />
+									<Route path="PvP/:id" element={<GamePvPWithRouter />} />
+									<Route path=":id" element={<ReviewGameWithRouter />} />
+								</Route>
+								<Route path='*' element={<NotFound />} />
+							</Route>
 						</Route>
 					</Routes>
 				</Compose>
--- src/components/layouts/CommonLayout.tsx
+++ src/components/layouts/CommonLayout.tsx
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
-import GamePvE from '../Game/GamePvE/GamePvE'
-import { GamePvPWithRouter } from '../Game/GamePvP/GamePvP/GamePvP'
-import { GamesListWithRouter } from '../Game/GamePvP/GamesList/GamesList'
-import PvPMenu from '../Game/GamePvP/PvPMenu/PvPMenu'
-import { ReviewGameWithRouter } from '../Game/ReviewGame/ReviewGame'
-import Home from '../Home/Home'
+
 import Nav from '../Nav/Nav'
-import NotFound from '../NotFound/NotFound'
-import Profile from '../Profile/Profile'
 
 export default class CommonLayout extends Component {
 	render() {
@@ -17,18 +10,7 @@ export default class CommonLayout extends Component {
 			<>
 				<Nav />
 				<main>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/profile' element={<Profile />} />
-						<Route path='/game' >
-							<Route path="PvE" element={<GamePvE />} />
-							<Route path="PvP" element={<PvPMenu />} />
-							<Route path="PvP/list" element={<GamesListWithRouter />} />
-							<Route path="PvP/:id" element={<GamePvPWithRouter />} />
-							<Route path=":id" element={<ReviewGameWithRouter />} />
-						</Route>
-						<Route path='*' element={<NotFound />} />
-					</Routes>
+					<Outlet />
 				</main >
 				<Footer />
 			</>
