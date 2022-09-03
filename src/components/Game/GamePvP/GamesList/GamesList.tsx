5d6ba74b3305ef3803b0c5dd8a996af92497dea8
NecroBread <mariovyordanov@gmail.com>
2022-09-03 15:00:44 +0300

    refactor: pvp games list

--- src/components/Game/GamePvP/GamesList/GamesList.tsx
+++ src/components/Game/GamePvP/GamesList/GamesList.tsx
@@ -1,121 +1,68 @@
-import { collection, doc, DocumentData, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAppSelector } from '../../../../app/hooks';
+import { doc, updateDoc } from 'firebase/firestore';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
 import { selectAuth } from '../../../../app/slices/authSlice';
+import { getList, selectList } from '../../../../app/slices/listSlice';
 import { db } from '../../../../configs/firebase.config';
+import { IGame } from '../../../../types/game.types';
 import Spinner from '../../../common/Spinner/Spinner';
 import styles from './GamesList.module.css';
 
-interface IState {
-	openGames: DocumentData[],
-	loading: boolean,
-	navigateTo: string,
-}
-
 const GamesList: React.FC = () => {
-	const [state, setState] = useState<IState>({
-		openGames: [],
-		loading: false,
-		navigateTo: '',
-	})
-
+	const list = useAppSelector(selectList);
 	const user = useAppSelector(selectAuth);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate()
 
 	useEffect(() => {
-		const ref = collection(db, 'activeGames');
-		const q = query(ref,
-			where("mode", "==", "pvp"),
-			where("open", "==", true),
-			orderBy("createdAt"),
-		);
+		dispatch(getList());
+	}, [dispatch])
 
-		getDocs(q)
-			.then((doc) => {
-				const result: DocumentData[] = []
-				doc.forEach(x => {
-					const data = x.data();
-					data.id = x.id;
-					result.unshift(data);
-				});
+	const handleJoinGame = (game: IGame) => {
+		if (user) {
+			const updatedProperties = {
+				open: false,
+				playerDisplayNames: [game.playerDisplayNames[0], user.displayName || 'Anonymous'],
+				playersIds: [game.playersIds[0], user.uid],
+			}
 
-				setState((st) => ({
-					...st,
-					openGames: result,
-				}))
-
-			})
-	}, [])
-
-	const handleJoinGame = (game: any) => {
-		// check if its already loading game 
-		if (state.loading === true) return;
-
-		setState((st) => ({
-			...st,
-			loading: true,
-		}))
-
-		const updatedGame = { ...game };
-		updatedGame.open = false;
-		updatedGame.playerDisplayNames[1] = user?.displayName || 'Anonymous';
-		updatedGame.playersIds[1] = user?.uid;
-
-		const ref = doc(db, "activeGames", game.id);
-		updateDoc(ref, updatedGame)
-			.then(() => {
-				setState((st) => ({
-					...st,
-					navigateTo: `/game/PvP/${game.id}`,
-				}))
-			})
-			.catch(err => {
-				// TODO ...
-				console.log(err);
-			})
-			.finally(() => {
-				setState((st) => ({
-					...st,
-					loading: false,
-				}))
-			})
-
+			const ref = doc(db, "activeGames", game.id);
+			updateDoc(ref, updatedProperties)
+				.then(() => {
+					navigate(`/game/PvP/${game.id}`);
+				})
+		}
 	}
 
-	if (state.navigateTo) {
-		return (
-			<Navigate to={state.navigateTo} />
-		)
-	} else {
-		return (
-			<div className={styles.wrapper}>
-				{state.loading
-					? <div className='absolute'>
-						<Spinner />
-					</div>
-					: null}
-				<h1 className='mb-3'>Open games</h1>
-				<table className={styles.table}>
-					<thead>
-						<tr>
-							<td>Date</td>
-							<td>Opponent</td>
-							<td></td>
+	return (
+		<div className={styles.wrapper}>
+			{list.status !== 'idle'
+				? <div className='absolute'>
+					<Spinner />
+				</div>
+				: null}
+			<h1 className='mb-3'>Open games</h1>
+			<table className={styles.table}>
+				<thead>
+					<tr>
+						<td>Date</td>
+						<td>Opponent</td>
+						<td></td>
+					</tr>
+				</thead>
+				<tbody>
+					{list.openGames.length > 0 && list.openGames.map(game => {
+						return <tr key={game.id} className={styles.row}>
+							<td className={styles.td}>{game.createdAt.split('T').join(', ').slice(0, -5)}</td>
+							<td className={styles.td}>{game.playerDisplayNames[0]}</td>
+							<td className={`${styles.td} ${styles.join}`} onClick={() => handleJoinGame(game as IGame)}>Join</td>
 						</tr>
-					</thead>
-					<tbody>
-						{state.openGames.map(game => {
-							return <tr key={game.id} className={styles.row}>
-								<td className={styles.td}>{game.createdAt.toDate().toISOString().split('T').join(', ').slice(0, -5)}</td>
-								<td className={styles.td}>{game.playerDisplayNames[0]}</td>
-								<td className={`${styles.td} ${styles.join}`} onClick={handleJoinGame.bind(this, game)}>Join</td>
-							</tr>
-						})}
-					</tbody>
-				</table>
-			</div>
-		)
-	}
+					})}
+				</tbody>
+			</table>
+		</div>
+	)
 }
 
 export default GamesList;
--- src/types/game.types.ts
+++ src/types/game.types.ts
@@ -1,3 +1,15 @@
 export type TSigns = 'x' | 'o' | undefined | null;
 export type TGameArray = TSigns[];
 export type THistoryArray = TSigns[][];
+export interface IGame {
+	id: string,
+	owner: string,
+	mode: 'pvp' | 'pve',
+	open: boolean,
+	history: THistoryArray,
+	playersIds: [string, string],
+	playerDisplayNames: [string, string],
+	playerSigns: ['x' | 'o', 'x' | 'o'],
+	winner: 'x' | 'o' | 'draw',
+	createdAt: string,
+}
